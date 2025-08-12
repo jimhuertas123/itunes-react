232cc673669856348b765d1822011e727bc35add
jimhuertas123 <jhuertas@unsa.edu.pe>
2025-08-12 14:36:01 -0500

    fix: reduce query api to only fetch music media

--- src/services/songService.ts
+++ src/services/songService.ts
@@ -5,7 +5,7 @@ import { iTunesApi } from "./apiClient";
 export async function fetchSongs(query: string): Promise<Array<Song>> {
     try {
         query;
-        const res = await fetch(`${iTunesApi}?term=${query}`);
+        const res = await fetch(`${iTunesApi}?term=${query}?media=music`);
         if (!res.ok) throw new Error('Network response was not ok');
         const dataJson = await res.json();
         if ('results' in dataJson) {
